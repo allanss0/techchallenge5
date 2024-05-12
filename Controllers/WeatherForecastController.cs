4c92b8aefaaa169b449928dbed87db9028db1f06
allanss0 <41849591+allanss0@users.noreply.github.com>
2024-05-12 13:49:06 -0300

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -30,33 +30,5 @@ namespace TechChallenge5.Controllers
             .ToArray();
         }
     }
-      [Route("[tempo]")]
-    public class tempoController : ControllerBase
-    {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing1", "Bracing1", "Chilly1", "Cool1", "Mild1", "Warm1", "Balmy1", "Hot1", "Sweltering1", "Scorching1"
-    };
 
-        private readonly ILogger<tempoController> _logger;
-
-        public WeatherForecastController(ILogger<tempoController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-       public ActionResult<IEnumerable<WeatherForecast>> Get()
-{
-    var rng = new Random();
-    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-    {
-        Date = DateTime.Now.AddDays(index),
-        TemperatureC = rng.Next(-20, 55),
-        Summary = Summaries[rng.Next(Summaries.Length)]
-    })
-    .ToArray();
-}
-        
-    }
 }
